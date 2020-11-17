9009c67621a8ad8940b8f0043f0505835d23b6e8
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-11-18 01:40:21 +0200

    created firebase service and added auth firestore and analytics

--- src/App.js
+++ src/App.js
@@ -10,21 +10,7 @@ import "firebase/analytics";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
-firebase.initializeApp({
-  // your config
-  apiKey: "AIzaSyDUQLyNq-lUpvV-RO6T5FgUhswc33Bj3TQ",
-  authDomain: "dreamers-1258e.firebaseapp.com",
-  databaseURL: "https://dreamers-1258e.firebaseio.com",
-  projectId: "dreamers-1258e",
-  storageBucket: "dreamers-1258e.appspot.com",
-  messagingSenderId: "292955652759",
-  appId: "1:292955652759:web:53f1162d3a618f8b919fd2",
-  measurementId: "G-32LJYNM1DQ",
-});
-
-const auth = firebase.auth();
-const firestore = firebase.firestore();
-const analytics = firebase.analytics();
+import { auth } from "./services/firebase";
 
 function App() {
   const [user] = useAuthState(auth);
